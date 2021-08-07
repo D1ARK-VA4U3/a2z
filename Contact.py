d4ac112a0848466992d998c0e005d9b381842812
D1ARK-VA4U3 <86417041+D1ARK-VA4U3@users.noreply.github.com>
2021-08-07 14:02:44 +0600

    Add files via upload

---
+++ Contact.py
@@ -0,0 +1,31 @@
+import os
+green="\033[0;32m"        # Green
+color_off="\033[0m"       # Text Reset
+print(green)
+print("""
+
+
+  __  __          ____            _             _   
+ |  \/  |_   _   / ___|___  _ __ | |_ __ _  ___| |_ 
+ | |\/| | | | | | |   / _ \| '_ \| __/ _` |/ __| __|
+ | |  | | |_| | | |__| (_) | | | | || (_| | (__| |_ 
+ |_|  |_|\__, |  \____\___/|_| |_|\__\__,_|\___|\__|
+         |___/                                      
+""")
+print ("\033[1;1;36m")
+print("Facebook Account: https://www.facebook.com/Arif.vau143")
+print ("\033[1;36m")
+print("Facebook Page: https://www.facebook.com/110596777934165/posts/129145789412597/")
+print ("\033[1;36m")
+
+print("Facebook Group Link:-https://www.facebook.com/groups/225764762211587/?ref=share")
+print ("\033[1;36m")
+
+
+print("Telegram Link:-https://t.me/D1arkva4u3")
+print ("\033[1;36m")
+
+
+print("Twitter Account Link:-https://twitter.com/AriyanNijhum?s=09")
+print ("\033[1;36m")
+print("web site link:-www.thedarkworldteam.blogspot.com/")
---
+++ txt_file.py
@@ -0,0 +1,22 @@
+print ("\033[0;32m")
+print("""
+
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+This Site Has been Hacked BY D1ARK-VA4U3. 
+
+
+=>i am D1RK-VA4U3<= =>Your security system is fuck :) <= <=WARNING=> {DEAR ADMIN} Fuck you're Website...! This Is Your Last Warning!!! So Fix Your Website Vulnerability :) And Next Time Be Careful. we are Bangladeshi Hacker we are systems maker. From BCA Team.
+
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""")
